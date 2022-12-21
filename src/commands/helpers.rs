ba9227a4d9c18b5631996ef8687a11f65dd81f7d
David Marchante <davidmarchan@gmail.com>
2022-12-20 21:08:34 -0500

    Extract open_editor_for(&Path)

---
+++ src/commands/helpers.rs
@@ -0,0 +1,34 @@
+use std::path::Path;
+use std::process::{Command, Stdio};
+
+use anyhow::bail;
+
+use super::sessions;
+
+pub fn open_editor_for(path: &Path) -> anyhow::Result<()> {
+    // Get the value of the $EDITOR environment variable
+    let editor = std::env::var("EDITOR").unwrap_or_else(|_| "vim".to_string());
+
+    let mut command = Command::new(editor);
+    command.arg(path);
+
+    // Set stdin, stdout, and stderr to be the same as the current process
+    command
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+
+    // Spawn the editor process and wait for it to finish
+    let status = command
+        .spawn()
+        .expect("Failed to spawn editor process")
+        .wait()
+        .expect("Failed to wait for editor process");
+
+    // Check the exit status of the editor process
+    if status.success() {
+        sessions::init()
+    } else {
+        bail!("Edit failed")
+    }
+}
--- src/commands/sessions/edit.rs
+++ src/commands/sessions/edit.rs
@@ -1,35 +1,6 @@
-use std::process::{Command, Stdio};
-
-use anyhow::bail;
-
+use crate::commands::helpers::open_editor_for;
 use crate::path;
 
 pub fn edit() -> anyhow::Result<()> {
-    let sessions_file = path::sessions_file();
-
-    // Get the value of the $EDITOR environment variable
-    let editor = std::env::var("EDITOR").unwrap_or_else(|_| "vim".to_string());
-
-    let mut command = Command::new(editor);
-    command.arg(sessions_file);
-
-    // Set stdin, stdout, and stderr to be the same as the current process
-    command
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit());
-
-    // Spawn the editor process and wait for it to finish
-    let status = command
-        .spawn()
-        .expect("Failed to spawn editor process")
-        .wait()
-        .expect("Failed to wait for editor process");
-
-    // Check the exit status of the editor process
-    if status.success() {
-        super::init()
-    } else {
-        bail!("Edit failed")
-    }
+    open_editor_for(&path::sessions_file())
 }
