45c158a086496f90612b3c447983d2485d14a21c
David Marchante <davidmarchan@gmail.com>
2025-10-09 01:10:59 -0400

    feat: make plugin list show installed status

--- src/commands/plugins/list.rs
+++ src/commands/plugins/list.rs
@@ -1,18 +1,27 @@
 use miette::Result;
 use owo_colors::OwoColorize;
 
-use crate::muxi::Settings;
+use crate::muxi::{Settings, path};
 
 pub fn list() -> Result<()> {
     let plugins = Settings::from_lua()?.plugins;
 
     if plugins.is_empty() {
         println!("{}", "No plugins defined!".red());
+        return Ok(());
     }
 
+    let plugins_dir = path::plugins_dir();
+
     for plugin in plugins {
-        // TODO: show if installed
-        println!("{}", plugin.green());
+        let install_path = plugin.install_path(&plugins_dir);
+        let repo_name = plugin.repo_name();
+
+        if install_path.exists() {
+            println!("{} {}", "✔".green().bold(), repo_name);
+        } else {
+            println!("{} {}", "○".dimmed(), repo_name.dimmed());
+        }
     }
 
     Ok(())
