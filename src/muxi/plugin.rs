81a0b426690e3bbd115dd05919381bc711c6e491
David Marchante <davidmarchan@gmail.com>
2025-10-26 11:59:29 -0400

    feat(plugins.init): use 'tmux run -b' to increase performance
    Delegating the execution of tmux plugin scripts to 'tmux run -b'
    improves performance by allowing tmux to handle the execution in the
    background, reducing the overhead of spawning new processes directly
    from the plugin manager.

--- src/muxi/plugin.rs
+++ src/muxi/plugin.rs
@@ -61,7 +61,10 @@ impl Plugin {
 
             // Execute all "*.tmux" files in the plugin directory
             if path.extension().is_some_and(|ext| ext == "tmux") {
-                let status = Command::new(&path)
+                let status = Command::new("tmux")
+                    .arg("run")
+                    .arg("-b")
+                    .arg(&path)
                     .stdout(std::process::Stdio::null())
                     .stderr(std::process::Stdio::null())
                     .status()
